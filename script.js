d24019cdcbb870afe8752142bd2077f9ac18ce09
rishulpu1 <rishulpu1@gmail.com>
2024-02-29 19:17:43 +0530

    built tabbed component

--- script.js
+++ script.js
@@ -58,3 +58,24 @@ navLinks.addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+//Tabbed Content
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabButtons = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  //Remove active classes
+  tabButtons.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Show Active Tab Content
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
